9a45cc59d52da2997bfa6b3f629379f017417817
WantClue <Lars_Naether@hotmail.de>
2024-03-19 22:17:31 +0100

    update

--- main/tasks/send_diagnostics_task.c
+++ main/tasks/send_diagnostics_task.c
@@ -1,38 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include "esp_log.h"
+#include "bm1397.h"
+#include "mining.h"
+#include "nvs_config.h"
+#include "serial.h"
+#include "DS4432U.h"
+#include "EMC2101.h"
+#include "INA260.h"
+#include "global_state.h"
+#include "esp_http_client.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_http_client.h"
-#include "esp_log.h"
-#include "cJSON.h"
-#include "global_state.h"
-
 
 void influxdb_sender_task(void *pvParameter) {
     ESP_LOGI("InfluxDBSender", "Task started");
 
     // Configure HTTP client for InfluxDB
     esp_http_client_config_t config = {
-        .url = "http://10.0.10.7.net:8086/api/v2/write?org=nther&bucket=bitaxe&precision=s",
+        .url = "http://10.0.10.7:8086/api/v2/write?org=nther&bucket=bitaxe&precision=s",
         .method = HTTP_METHOD_POST,
-        .headers = "Authorization: Token VvA66ZLtvF-RcytZivMPqG7RThi2RZ_itxleXKsiw0wfxM3e3bTgBbrbi6mmUPFz84G8lNTcdCO3nHME1RSS5w==",
     };
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+    esp_http_client_set_header(client, "Authorization", "Token VvA66ZLtvF-RcytZivMPqG7RThi2RZ_itxleXKsiw0wfxM3e3bTgBbrbi6mmUPFz84G8lNTcdCO3nHME1RSS5w==");
+    
+    GlobalState * GLOBAL_STATE = (GlobalState *) pvParameters;
+
 
     while (1) {
         // Collect data
         char data[512];
         snprintf(data, sizeof(data),
                  "system_info board_version=\"%s\",temp=%.2f,hashRate=%llu,coreVoltage=%.2f,coreVoltageActual=%.2f,frequency=%u,version=\"%s\"",
-                 GLOBAL_STATE->board_version,
+                 //GLOBAL_STATE->board_version,
                  GLOBAL_STATE->POWER_MANAGEMENT_MODULE.chip_temp,
                  GLOBAL_STATE->SYSTEM_MODULE.current_hashrate,
                  GLOBAL_STATE->POWER_MANAGEMENT_MODULE.voltage,
-                 GLOBAL_STATE->SYSTEM_MODULE.current
-                 ADC_get_vcore(),  
-                 GLOBAL_STATE->ASIC_FREQ, 
-                 esp_app_get_description()->version  // Assuming this gets the firmware version
-                 );
+                 //ADC_get_vcore(),  // Assuming this gets the actual core voltage
+                 GLOBAL_STATE->ASIC_FREQ,  // Assuming this is how you get the ASIC frequency
+                 //esp_app_get_description()->version  // Assuming this gets the firmware version
+        );
 
-        // Send data
-        esp_http_client_handle_t client = esp_http_client_init(&config);
         esp_http_client_set_post_field(client, data, strlen(data));
         esp_err_t err = esp_http_client_perform(client);
         if (err == ESP_OK) {
@@ -42,6 +50,6 @@ void influxdb_sender_task(void *pvParameter) {
         }
         esp_http_client_cleanup(client);
 
-        vTaskDelay(pdMS_TO_TICKS(300000));  // 300 seconds delay
+        vTaskDelay(pdMS_TO_TICKS(30000));  // 30 seconds delay
     }
 }
