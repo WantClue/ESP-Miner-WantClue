1edd3ff92e19e43f842d531ce9788b85b0b9f153
WantClue <wantclue@users.noreply.github.com>
2025-12-07 11:58:56 +0100

    typo

--- main/bap/bap_handlers.c
+++ main/bap/bap_handlers.c
@@ -238,7 +238,7 @@ void BAP_send_request(bap_parameter_t param, GlobalState *state) {
             BAP_send_message(BAP_CMD_RES, "poolUser", state->SYSTEM_MODULE.pool_user);
             break;
         case BAP_PARAM_SHARES:
-            BAP_send_message(BAP_CMD_RES, "sharesAccepted", state->SYSTEM_MODULE.shares_accepted_str);
+            BAP_send_message(BAP_CMD_RES, "sharesAccepted", state->SYSTEM_MODULE.shares_accepted);
             break;
         default:
             ESP_LOGE(TAG, "Unsupported request parameter: %d", param);
